45462e3333989fc64dce41512ff11ab2d67f8c0d
Joshua Shaffer <xeio@live.com>
2017-11-11 15:04:16 -0500

    Filter items displayed on the home page

--- Website/SwlMarket/Controllers/HomeController.cs
+++ Website/SwlMarket/Controllers/HomeController.cs
@@ -24,13 +24,45 @@ namespace SwlMarket.Controllers
             var x = _marketContext.Prices
                 .FromSql("select * from Prices where Id in (select max(Id) from Prices group by ItemId)")
                 .Include(p => p.Item)
+                .Where(p => ShowOnHome(p.Item))
                 .OrderBy(p => p.Item.Name)
                 .AsNoTracking()
                 .ToListAsync();
 
-            //var y = await _marketContext.Prices.Where(p => _marketContext.Items.Any(i => i.ID == p.ItemID)).ToListAsync();
-
             return View(await x);
+        }
+
+        private bool ShowOnHome(Item item)
+        {
+            switch (item.ItemCategory)
+            {
+                case ItemCategory.AssaultRifle:
+                case ItemCategory.Blade:
+                case ItemCategory.BloodMagicFocus:
+                case ItemCategory.ChaosFocus:
+                case ItemCategory.ElementalismFocus:
+                case ItemCategory.FistWeapon:
+                case ItemCategory.Hammer:
+                case ItemCategory.Pistols:
+                case ItemCategory.Shotgun:
+                    if (item.Rarity < Rarity.Superior) return false;
+                    if (item.Name.EndsWith("Mk I")) return false;
+                    return item.IsExtraordinary ?? false;
+                case ItemCategory.FingerTalisman:
+                case ItemCategory.HeadTalisman:
+                case ItemCategory.LuckTalisman:
+                case ItemCategory.NeckTalisman:
+                case ItemCategory.OccultTalisman:
+                case ItemCategory.WaistTalisman:
+                case ItemCategory.WristTalisman:
+                    if (item.Name.StartsWith("Faded ")) return false;
+                    return item.IsExtraordinary ?? false;
+                case ItemCategory.Glyph:
+                    return item.Name.StartsWith("Intricate ");
+                case ItemCategory.Gadget:
+                    return item.Rarity >= Rarity.Mythic;
+            }
+            return true;
         }
 
         public IActionResult About()
