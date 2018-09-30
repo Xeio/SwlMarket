61860810813759492325fa865399025e694e22a2
Joshua Shaffer <xeio@live.com>
2018-09-30 19:19:29 -0400

    Show last updated ago rather than expire time

--- Website/SwlMarket/wwwroot/js/site.js
+++ Website/SwlMarket/wwwroot/js/site.js
@@ -1,1 +1,20 @@
-﻿// Write your JavaScript code.
+﻿window.onload = () => updateTimeSince();
+
+function updateTimeSince() {
+    let current = new Date().getTime();
+    for (let element of document.getElementsByClassName("timesince")) {
+        let timestamp = Date.parse(element.textContent);
+        let minutesDiff = (current - timestamp) / 1000 / 60;
+        if (minutesDiff < 60) {
+            element.textContent = `${Math.trunc(minutesDiff)} minutes ago`;
+            continue;
+        }
+        let hoursDiff = minutesDiff / 60;
+        if (hoursDiff < 24) {
+            element.textContent = `${Math.trunc(hoursDiff)} hours ago`;
+            continue;
+        }
+        let daysAgo = hoursDiff / 24;
+        element.textContent = `${Math.trunc(daysAgo)} days ago`;
+    }
+}
