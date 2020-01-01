f3bd508eb8d4c3d659bedac6e166fb720461ff07
Joshua Shaffer <xeio@live.com>
2020-01-01 18:01:37 -0500

    Add migration for MostRecentPrice trigger

---
+++ Website/SwlMarket/Migrations/20200101225949_AddMostRecentPriceTrigger.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SwlMarket.Migrations
+{
+    public partial class AddMostRecentPriceTrigger : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			migrationBuilder.Sql(@"CREATE TRIGGER Trigger_UpdateMostRecentPrice
+									AFTER INSERT
+									ON prices
+									FOR EACH ROW
+									BEGIN
+										IF EXISTS(SELECT 1 FROM MostRecentPrices WHERE MostRecentPrices.ItemId = NEW.ItemId) THEN
+											UPDATE MostRecentPrices
+											SET MostRecentPrices.Time = NEW.Time, MostRecentPrices.Marks = NEW.Marks, MostRecentPrices.IPId = NEW.IPId
+											WHERE MostRecentPrices.ItemId = NEW.ItemID;
+										ELSE
+											INSERT INTO MostRecentPrices (ItemId, Time, Marks, IPId) 
+											VALUES (NEW.ItemId, NEW.Time, NEW.Marks, NEW.IPId);
+										END IF;
+									END");
+
+			migrationBuilder.Sql(@"INSERT INTO mostrecentprices (ItemId, Time, Marks, IPId)
+									SELECT ItemId, Time, Marks, IPId
+									FROM Prices
+									WHERE Id in (select max(Id) from prices group by ItemId) AND
+										ItemId NOT IN (SELECT ItemId FROM MostRecentPrices)");
+		}
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+			migrationBuilder.Sql("DROP TRIGGER Trigger_UpdateMostRecentPrice");
+		}
+    }
+}
