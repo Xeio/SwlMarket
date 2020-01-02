37e99412745b6d1fb0ddf344cfd3765525acbf85
Joshua Shaffer <xeio@live.com>
2020-01-01 19:05:19 -0500

    Remove database project

--- Website/SwlMarketDb/Scripts/PopulateMostRecentPricesTable.sql
+++
@@ -1,5 +0,0 @@
-﻿INSERT INTO MostRecentPrices (ItemId, [Time], Marks, IPId)
-SELECT ItemId, [Time], Marks, IPId
-FROM Prices
-WHERE Id in (select max(Id) from Prices group by ItemId) AND
-      ItemId NOT IN (SELECT ItemId FROM MostRecentPrices)
